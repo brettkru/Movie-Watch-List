af3c13ab4346d1d306c8d38583e16f45d5183bdc
krudoppler <bkru33@gmail.com>
2025-02-16 13:25:32 -0500

    chill

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Global variables
 const apiKey = `523b9fd9`
-const searchTerm = "Inception"
-const url = `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+const searchTerm = "parasite"
+const url = `http://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`
 
 fetch(url)
     .then(r => r.json())
